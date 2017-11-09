514821262516f55a476f49b86c6018fa60889a23
carleton <cjc@lanl.gov>
2017-11-09 09:27:13 -0700

    adding solver settings to minlpbnb

--- minlpbnb.jl
+++ minlpbnb.jl
@@ -26,6 +26,25 @@ function main(parsed_args)
         solver_args[:time_limit] = parsed_args["time-limit"]
     end
 
+    if parsed_args["branch_strategy"] != nothing
+        solver_args[:branch_strategy] = Symbol(parsed_args["branch_strategy"])
+    end
+
+    if parsed_args["strong_restart"] != nothing
+        solver_args[:strong_restart] = parsed_args["strong_restart"]
+    end
+
+    if parsed_args["traverse_strategy"] != nothing
+        solver_args[:traverse_strategy] = Symbol(parsed_args["traverse_strategy"])
+    end
+
+    if parsed_args["processors"] != nothing
+        addprocs(parsed_args["processors"])
+        solver_args[:processors] = parsed_args["processors"]
+    end
+
+
+
     solver = MINLPBnBSolver(nlp_solver; solver_args...)
     setsolver(m, solver)
 
@@ -34,6 +53,31 @@ function main(parsed_args)
     print_result(m, status, parsed_args["file"])
 end
 
+function parse_commandline_bnb()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--file", "-f"
+            help = "the minlplib data file (.jl)"
+            required = true
+        "--time-limit", "-t"
+            help = "puts a time limit on the sovler"
+            arg_type = Float64
+        "--branch_strategy"
+            help = "branch strategy"
+        "--strong_restart"
+            help = "strong branching option"
+            action = :store_true
+        "--traverse_strategy"
+            help = "traverse strategy"
+        "--processors", "-p"
+            help = "traverse strategy"
+            arg_type = Int
+    end
+
+    return parse_args(s)
+end
+
 if isinteractive() == false
-  main(parse_commandline())
+  main(parse_commandline_bnb())
 end
