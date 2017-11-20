7008fdc326a7445381d85de08962c7b7bc91ace1
carleton coffrin <cjc@lanl.gov>
2017-11-20 15:39:03 -0700

    adding nl writer variants of bonmin and couenne

---
+++ couenne-nlw.jl
@@ -0,0 +1,33 @@
+#!/usr/bin/env julia
+
+using ArgParse
+
+using AmplNLWriter
+
+include("common.jl")
+
+function main(parsed_args) 
+    include(parsed_args["file"])
+    # this adds a model named m to the current scope
+
+    if parsed_args["time-limit"] != nothing
+        tl = parsed_args["time-limit"]
+
+        # WARNING this is not parrallel safe
+        open("couenne.opt", "w") do f
+            write(f, "time_limit $(tl)")
+        end
+    end
+
+    solver = AmplNLSolver("couenne")
+
+    setsolver(m, solver)
+
+    status = solve(m)
+
+    print_result(m, status, parsed_args["file"])
+end
+
+if isinteractive() == false
+  main(parse_commandline())
+end
