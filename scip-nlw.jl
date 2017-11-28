c66f42d0af298beafb780499c242962fe6d16927
Carleton Coffrin <carleton@coffr.in>
2017-11-27 18:49:49 -0700

    Adding SCIP via NLWriter (#3)
    * adding scip via AmplNLWriter

---
+++ scip-nlw.jl
@@ -0,0 +1,45 @@
+#!/usr/bin/env julia
+
+using ArgParse
+
+using AmplNLWriter
+
+include("common.jl")
+
+function main(parsed_args) 
+    include(parsed_args["file"])
+    # this adds a model named m to the current scope
+
+    lines = String[]
+
+    append!(lines, ["heuristics/subnlp/nlpoptfile = \"ipopt.opt\""])
+
+    if parsed_args["time-limit"] != nothing
+        tl = parsed_args["time-limit"]
+        append!(lines, ["limits/time = $(tl)"])
+    end
+
+    if parsed_args["print-level"] != nothing
+        pl = parsed_args["print-level"]
+        append!(lines, ["display/verblevel = $(pl)"])
+    end
+
+    # WARNING this is not parrallel safe
+    open("scip.set", "w") do f
+        for line in lines
+            write(f, "$(line) \n")
+        end
+    end
+
+    solver = AmplNLSolver("scipampl")
+
+    setsolver(m, solver)
+
+    status = solve(m)
+
+    print_result(m, status, parsed_args["file"])
+end
+
+if isinteractive() == false
+  main(parse_commandline())
+end
