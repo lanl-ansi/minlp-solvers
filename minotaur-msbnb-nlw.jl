06fea17cff40bc071d207b409ac63e1dc72c4136
carleton coffrin <cjc@lanl.gov>
2018-01-26 16:37:15 -0700

    adding fpump to minotaur msbnb

--- minotaur-msbnb-nlw.jl
+++ minotaur-msbnb-nlw.jl
@@ -15,8 +15,9 @@ function main(parsed_args)
     append!(optoins, ["--obj_gap_percent"])
     append!(optoins, ["1e-2"])
 
-    #append!(optoins, ["--nlp_engine"])
-    #append!(optoins, ["IPOPT"])
+    # seems to break things
+    #append!(optoins, ["--fpump"])
+    #append!(optoins, ["1"])
 
     if parsed_args["time-limit"] != nothing
         tl = parsed_args["time-limit"]
