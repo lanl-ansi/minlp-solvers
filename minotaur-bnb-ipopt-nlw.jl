cb70007b348769fbc2711f75ba57572a2d4af814
carleton coffrin <cjc@lanl.gov>
2018-01-26 15:48:03 -0700

    adding minotaur solver nlw connectors

---
+++ minotaur-bnb-ipopt-nlw.jl
@@ -0,0 +1,45 @@
+#!/usr/bin/env julia
+
+using ArgParse
+
+using JuMP
+using AmplNLWriter
+
+include("common.jl")
+
+function main(parsed_args) 
+    include(parsed_args["file"])
+    # this adds a model named m to the current scope
+
+    optoins = String[]
+    append!(optoins, ["--obj_gap_percent"])
+    append!(optoins, ["1e-2"])
+
+    append!(optoins, ["--nlp_engine"])
+    append!(optoins, ["IPOPT"])
+
+    if parsed_args["time-limit"] != nothing
+        tl = parsed_args["time-limit"]
+        append!(optoins, ["--bnb_time_limit"])
+        append!(optoins, ["$(tl)"])
+    end
+
+    if parsed_args["print-level"] != nothing
+        pl = parsed_args["print-level"]
+        append!(optoins, ["--log_level"])
+        append!(optoins, ["$(pl)"])
+    end
+
+    solver = AmplNLSolver("bnb", optoins)
+
+    setsolver(m, solver)
+
+    status = solve(m)
+
+    print_result(m, status, parsed_args["file"])
+end
+
+if isinteractive() == false
+  main(parse_commandline())
+end
+
